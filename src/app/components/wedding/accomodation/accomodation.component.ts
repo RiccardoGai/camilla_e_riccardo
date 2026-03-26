1486b70c16222deaddcdc02f1ecfa6800da38021
Riccardo Gai <riccardo.gai91@gmail.com>
2026-03-26 10:59:33 +0100

    Revert "accomodation"
    This reverts commit ff3ac0b1a6f2b3c4c51d9f7db73d658b4fb5a00b.

--- src/app/components/wedding/accomodation/accomodation.component.ts
+++ src/app/components/wedding/accomodation/accomodation.component.ts
@@ -1,5 +1,14 @@
+
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { hugeGuestHouse, hugeHome01, hugeHotel01 } from '@ng-icons/huge-icons';
 import { AnimateOnScrollDirective } from '../../../directives/animate-on-scroll.directive';
+
+interface Hotel {
+  name: string;
+  link: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-accomodation',
@@ -12,6 +21,25 @@ import { AnimateOnScrollDirective } from '../../../directives/animate-on-scroll.
     `,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AnimateOnScrollDirective],
+  imports: [AnimateOnScrollDirective, NgIcon],
+  providers: [provideIcons({ hugeHotel01, hugeGuestHouse, hugeHome01 })],
 })
-export class AccomodationComponent {}
+export class AccomodationComponent {
+  hotels = [
+    {
+      name: 'Re Artù',
+      link: 'https://reartuassisi.it/',
+      icon: 'hugeHotel01',
+    },
+    {
+      name: 'Casale Merlino',
+      link: 'https://reartuassisi.it/ospitalita/merlino-il-casale',
+      icon: 'hugeGuestHouse',
+    },
+    {
+      name: 'Antica Fonte',
+      link: 'https://www.anticafonteassisi.com/',
+      icon: 'hugeHome01',
+    },
+  ];
+}
